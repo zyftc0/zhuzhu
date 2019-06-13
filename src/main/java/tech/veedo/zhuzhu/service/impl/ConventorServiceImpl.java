bd29f92721c2f38100c70d7b9935e7d40c08c4f1
ifzhang <506898287@qq.com>
2019-06-14 00:45:00 +0800

    生成了正常的docx

--- src/main/java/tech/veedo/zhuzhu/service/impl/ConventorServiceImpl.java
+++ src/main/java/tech/veedo/zhuzhu/service/impl/ConventorServiceImpl.java
@@ -6,14 +6,21 @@ import freemarker.template.TemplateException;
 import freemarker.template.Version;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 import tech.veedo.zhuzhu.entity.CandidateReport;
 import tech.veedo.zhuzhu.entity.Compensation;
 import tech.veedo.zhuzhu.entity.PersonalInformation;
+import tech.veedo.zhuzhu.entity.Title;
 import tech.veedo.zhuzhu.service.ConventorService;
 import tech.veedo.zhuzhu.utils.MyUtils;
 
 import java.io.*;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 @Service
 public class ConventorServiceImpl implements ConventorService {
@@ -22,6 +29,9 @@ public class ConventorServiceImpl implements ConventorService {
     private String templatePath;
 
     public static void main(String[] args) throws IOException, TemplateException {
+        Title title = new Title()
+                .setPost("八十万禁军教头")
+                .setPostLocation("汴梁");
         PersonalInformation pi = new PersonalInformation()
                 .setName("盖伦")
                 .setGender(0)
@@ -34,57 +44,69 @@ public class ConventorServiceImpl implements ConventorService {
                 .setPersonalInformation(pi)
                 .setCompensation(compensation);
 
-        // 模板文件路径：
-        String templetFilePath = "classpath:templates/template.xml";
-        // 目标文件存放路径
-        String targetFilePath = "D:/测试模板(xml格式).doc";
+
         HashMap<String, Object> total = new HashMap<>();
+        HashMap<String, Object> titleMap = MyUtils.Object2HashMap(title);
         HashMap<String, Object> piMap = MyUtils.Object2HashMap(pi);
         HashMap<String, Object> compensationMap = MyUtils.Object2HashMap(compensation);
+        total.putAll(titleMap);
         total.putAll(piMap);
         total.putAll(compensationMap);
 
-        xml2XmlDoc(piMap, templetFilePath, targetFilePath);
-    }
+        // 模板文件路径：
+        String templateFilePath = "classpath:templates/";
+        String templateDocxName = "document.docx";
+        String templateXmlName = "document.xml";
 
-    /**
-     * 将xml模板转换为后缀为doc文件，本质仍是属于xml
-     * @param dataMap	需要填充到模板的数据
-     * @param templetFilePath	模板文件路径
-     * @param targetFilePath	目标文件保存路径
-     * @throws IOException
-     * @throws TemplateException
-     */
-    public static void xml2XmlDoc(HashMap<String, Object> dataMap, String templetFilePath, String targetFilePath) throws IOException, TemplateException {
-        // 将模板文件路径拆分为文件夹路径和文件名称
-//        String tempLetDir = templetFilePath.substring(0,templetFilePath.lastIndexOf("/"));
-        // 注意：templetFilePath.lastIndexOf("/")中，有的文件分隔符为：\ 要注意文件路径的分隔符
-        String templetName = templetFilePath.substring(templetFilePath.lastIndexOf("/")+1);
-        // 将目标文件保存路径拆分为文件夹路径和文件名称
-        String targetDir = targetFilePath.substring(0,targetFilePath.lastIndexOf("/"));
-        String targetName = targetFilePath.substring(targetFilePath.lastIndexOf("/")+1);
+        // 目标文件存放路径
+        String outDirPath = "/Users/ifzhang/Downloads/";
+        String tmpXmlName = "temp.xml";
+        String finalDocxName = "final.docx";
 
-        // 如果目标文件目录不存在，则需要创建
-        File file = new File(targetDir);
-        if(!file.exists()){
-            file.mkdirs();
+        {
+            File templateXmlPath = ResourceUtils.getFile(templateFilePath);
+            Configuration configuration = new Configuration(new Version("2.3.0"));
+            configuration.setDefaultEncoding("UTF-8");
+            // 加载模板数据（从文件路径中获取文件，其他方式，可百度查找）
+            configuration.setDirectoryForTemplateLoading(templateXmlPath);
+            // 获取模板实例
+            Template template = configuration.getTemplate(templateXmlName);
+            template.setOutputEncoding("UTF-8");
+
+            //将模板和数据模型合并生成文件
+            Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(outDirPath + tmpXmlName)),"UTF-8"));
+            //生成xmldocx文件
+            template.process(total, out);
+            out.flush();
+            out.close();
         }
 
-        Version version = new Version("2.3.0");
-        Configuration configuration = new Configuration(version);
-        configuration.setDefaultEncoding("UTF-8");
-        // 加载模板数据（从文件路径中获取文件，其他方式，可百度查找）
-        configuration.setDirectoryForTemplateLoading(new File(templetFilePath));
-        // 获取模板实例
-        Template template = configuration.getTemplate(templetName);
-        File outFile = new File(targetDir + File.separator + targetName);
-        //将模板和数据模型合并生成文件
-        Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile),"UTF-8"));
-
-        //生成文件
-        template.process(dataMap, out);
-        out.flush();
-        out.close();
+        File file = new File(outDirPath + tmpXmlName);
+        File docxFile = ResourceUtils.getFile(templateFilePath + templateDocxName);
+        ZipFile zipFile = new ZipFile(docxFile);
+        Enumeration<? extends ZipEntry> zipEntrys = zipFile.entries();
+        ZipOutputStream zipout = new ZipOutputStream(new FileOutputStream(outDirPath + finalDocxName));
+        int len;
+        byte[] buffer = new byte[1024];
+        while (zipEntrys.hasMoreElements()) {
+            ZipEntry next = zipEntrys.nextElement();
+            InputStream is = zipFile.getInputStream(next);
+            // 把输入流的文件传到输出流中 如果是word/document.xml由我们输入
+            zipout.putNextEntry(new ZipEntry(next.toString()));
+            if ("word/document.xml".equals(next.toString())) {
+                InputStream in = new FileInputStream(file);
+                while ((len = in.read(buffer)) != -1) {
+                    zipout.write(buffer, 0, len);
+                }
+                in.close();
+            } else {
+                while ((len = is.read(buffer)) != -1) {
+                    zipout.write(buffer, 0, len);
+                }
+                is.close();
+            }
+        }
+        zipout.close();
     }
 
 }
